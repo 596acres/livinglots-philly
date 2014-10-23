b59d0fafdd00800d5723a0373138d05bc0cff16d
Eric Brelsford <ebrelsford@gmail.com>
2014-10-23 10:22:32 -0400

    Link to map for expressing an interest

--- livinglotsphilly/lots/templatetags/lot_tags.py
+++ livinglotsphilly/lots/templatetags/lot_tags.py
@@ -6,6 +6,14 @@ from django import template
 register = template.Library()
 
 
+def express_interest_link(lot):
+    point = lot.centroid.transform(3652, clone=True)
+    return 'http://secure.phila.gov/PAPLPublicWeb/Mapview.aspx?_X=%f&_Y=%f' % (
+        point.x,
+        point.y
+    )
+
+
 def main_map_url(lot):
     default_filters = ('parents_only=True&known_use_existence=not+in+use&'
                        'known_use_existence=in+use&'
@@ -20,4 +28,6 @@ def main_map_url(lot):
     })
     return '/?%s&centroid=%s&zoom=18' % (default_filters, centroid)
 
+
+register.filter('express_interest_link', express_interest_link)
 register.filter('main_map_url', main_map_url)
