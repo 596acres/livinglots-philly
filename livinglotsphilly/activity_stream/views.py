1b7750491284bc05409c985802ba5758775617cc
Eric Brelsford <ebrelsford@gmail.com>
2014-12-11 12:11:51 -0500

    Include tweets in news feed

--- livinglotsphilly/activity_stream/views.py
+++ livinglotsphilly/activity_stream/views.py
@@ -7,6 +7,7 @@ from django.views.generic import View
 from actstream.models import Action
 from braces.views import JSONResponseMixin
 from elephantblog.models import Entry
+from twitter_feed.models import Tweet
 
 
 def _clean_html(html):
@@ -50,8 +51,18 @@ class CombinedActivityFeed(JSONResponseMixin, View):
         return [as_dict(e) for e in Entry.objects.active()]
 
     def get_tweets(self):
-        # TODO
-        return []
+        def as_html(tweet):
+            return _clean_html(render_to_string('twitter_feed/tweet_activity_stream.html',
+                                                { 'tweet': tweet }))
+
+        def as_dict(tweet):
+            return {
+                'html': as_html(tweet),
+                'time': tweet.published_at,
+                'type': 'tweet',
+            }
+
+        return [as_dict(e) for e in Tweet.objects.all()]
 
     def get_activities(self, page=1, per_page=20):
         """Get combined, sorted activities, paginated"""
