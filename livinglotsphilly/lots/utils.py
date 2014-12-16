c842f3e61acdeb9a53c6d579632f0f7f5c850b6c
Eric Brelsford <ebrelsford@gmail.com>
2014-12-16 13:15:50 -0500

    Fix WaterParcels for lots

---
+++ livinglotsphilly/lots/utils.py
@@ -0,0 +1,28 @@
+from phillydata.waterdept.models import WaterParcel
+
+from .models import Lot
+
+
+def _fix_water_parcel(lot):
+    # Find and set to new one
+    lot.water_parcel = WaterParcel.objects.get(parcelid=lot.water_parcel.parcel_id)
+
+    # Update geometry
+    lot.polygon = lot.water_parcel.geometry
+    lot.centroid = lot.polygon.centroid
+    lot.save()
+
+
+def fix_water_parcels(count=1000):
+    """
+    Fix lots' water parcels.
+
+    Point to new water parcels where available since these have geometries and
+    are generally more updated.
+    """
+    lots_with_old_water_parcels = Lot.objects.filter(
+        water_parcel__isnull=False,
+        water_parcel__parcelid__isnull=True,
+    )
+    for lot in lots_with_old_water_parcels[:count]:
+        _fix_water_parcel(lot)
