0ee2223fb4b39cb12235e70276a90cedc3e627ef
Eric Brelsford <ebrelsford@gmail.com>
2014-12-22 11:33:28 -0500

    Set fields in GroundtruthRecordForm

--- livinglotsphilly/groundtruth/forms.py
+++ livinglotsphilly/groundtruth/forms.py
@@ -8,6 +8,16 @@ from .models import GroundtruthRecord
 class GroundtruthRecordForm(GroundtruthRecordFormMixin, forms.ModelForm):
 
     class Meta:
+        fields = (
+            'content_type',
+            'object_id',
+            'actual_use',
+            'contact_name',
+            'contact_email',
+            'contact_phone',
+            'use',
+        )
+
         model = GroundtruthRecord
         widgets = {
             'content_type': forms.HiddenInput(),
