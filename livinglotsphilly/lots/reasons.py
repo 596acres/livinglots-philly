eb49fd47ec64d86e508b235046ff3ab188bd3b90
Eric Brelsford <ebrelsford@gmail.com>
2015-02-10 12:17:13 -0500

    Try not to choke on permeability check

--- livinglotsphilly/lots/reasons.py
+++ livinglotsphilly/lots/reasons.py
@@ -33,12 +33,17 @@ def get_reasons(lot):
             'short': 'L&I licenses',
             'long': mark_safe('It has <a href="http://www.phila.gov/data/Pages/LIPropertyHistory.aspx?entity=locationhistory&eid=%s" target="_blank">licenses from Licensing and Inspections</a> that indicate that it is vacant.' % lot.licenses.all()[0].location.external_id),
         })
-    if lot.water_parcel and lot.water_parcel.percent_permeable > 50:
-        reasons.append({
-            'short': 'PWD mostly permeable',
-            'long': mark_safe('The Water Department <a href="http://www.phila.gov/water/swmap/Parcel.aspx?parcel_id=%s" target="_blank">claims</a> that the parcel is %s%% permeable (without structures or pavement).' % (
-                lot.water_parcel.parcel_id,
-                lot.water_parcel.percent_permeable,
-            ))
-        })
+
+    try:
+        if lot.water_parcel and lot.water_parcel.percent_permeable > 50:
+            reasons.append({
+                'short': 'PWD mostly permeable',
+                'long': mark_safe('The Water Department <a href="http://www.phila.gov/water/swmap/Parcel.aspx?parcel_id=%s" target="_blank">claims</a> that the parcel is %s%% permeable (without structures or pavement).' % (
+                    lot.water_parcel.parcel_id,
+                    lot.water_parcel.percent_permeable,
+                ))
+            })
+    except TypeError:
+        pass
+
     return reasons
